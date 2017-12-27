87a79cd859a3b80aedd2506131b3219e90d4f422
qimenggao <qi7210577@qq.com>
2017-12-27 14:11:03 +0800

    Initial commit

---
+++ src/main/java/com/e3mall/search/controller/SearchController.java
@@ -0,0 +1,38 @@
+package com.e3mall.search.controller;
+
+import com.e3mall.pojo.SearchResult;
+import com.e3mall.search.service.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by qimenggao on 2017/12/26.
+ */
+@Controller
+public class SearchController {
+    @Autowired
+    private SearchService searchService;
+    @Value("${PAGE_ROWS}")
+    private Integer PAGE_ROWS;
+
+    @RequestMapping("search")
+    public String searchResult(Model model,String keyword, @RequestParam(defaultValue = "1") Integer page) throws Exception {
+            //需要转码
+            keyword = new String(keyword.getBytes("iso8859-1"), "utf-8");
+            //调用Service查询商品信息
+            SearchResult result = searchService.search(keyword, page, PAGE_ROWS);
+            //把结果传递给jsp页面
+            model.addAttribute("query", keyword);
+            model.addAttribute("totalPages", result.getTotalPages());
+            model.addAttribute("recordCount", result.getRecordCount());
+            model.addAttribute("page", page);
+            model.addAttribute("itemList", result.getItemList());
+            //返回逻辑视图
+            return "search";
+    }
+
+}
